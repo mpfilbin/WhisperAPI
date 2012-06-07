c1cd0ea4ae4350c68f1b09b4199e0fb485415d66
Eric Bowen <github@scrappydog.com>
2012-06-07 16:16:29 -0600

    refactoring pearson api related code and additional test cases

--- Whisper.API/Whisper.API/Controllers/HomeController.cs
+++ Whisper.API/Whisper.API/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Net;
 using System.IO;
 using System.Text;
 using System.Web.Script.Serialization;
+using Whisper.API.Utilities;
 
 namespace Whisper.API.Controllers
 {
@@ -43,137 +44,19 @@ namespace Whisper.API.Controllers
 
         public ActionResult OAuthTest()
         {
+            const string username = "jhNXTprof";
+            const string password = "jhNXTprof";
 
-            string username = "jhNXTprof";
-            string password = "jhNXTprof";
+            var accessToken = PearsonApiUtilities.GetOauthAccessToken(username, password);
 
-            // Setup the variables necessary to make the Request 
-            string grantType = "password";
-            string clientID = "814c9ce7-664a-412f-a830-ff96269b0613";
-            string clientString = "sullyu";
-
-            string url = "https://m-api.ecollege.com/token";
-            HttpWebResponse response = null;
-
-            try
-            {
-                // Create the data to send
-                StringBuilder data = new StringBuilder();
-                data.Append("grant_type=" + Uri.EscapeDataString(grantType));
-                data.Append("&client_id=" + Uri.EscapeDataString(clientID));
-                data.Append("&username=" + Uri.EscapeDataString(clientString + "\\" + username));
-                data.Append("&password=" + Uri.EscapeDataString(password));
-
-                // Create a byte array of the data to be sent
-                byte[] byteArray = Encoding.UTF8.GetBytes(data.ToString());
-
-                // Setup the Request
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                request.Method = "POST";
-                request.ContentType = "application/x-www-form-urlencoded";
-                request.ContentLength = byteArray.Length;
-
-                // Write data
-                Stream postStream = request.GetRequestStream();
-                postStream.Write(byteArray, 0, byteArray.Length);
-                postStream.Close();
-
-                // Send Request & Get Response
-                response = (HttpWebResponse)request.GetResponse();
-
-                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-                {
-                    // Get the Response Stream
-                    string json = reader.ReadLine();
-                    Console.WriteLine(json);
-
-                    // Retrieve and Return the Access Token
-                    JavaScriptSerializer ser = new JavaScriptSerializer();
-                    Dictionary<string, object> x = (Dictionary<string, object>)ser.DeserializeObject(json);
-                    string accessToken = x["access_token"].ToString();
-
-                    ViewBag.OAuthResult = accessToken;
-
-                    ViewBag.MeCoursesResult = XAuthHeaderTest(accessToken);
-                }
-            }
-            catch (WebException e)
-            {
-                // This exception will be raised if the server didn't return 200 - OK
-                // Retrieve more information about the error
-                if (e.Response != null)
-                {
-                    using (HttpWebResponse err = (HttpWebResponse)e.Response)
-                    {
-                        ViewBag.Result = string.Format ("The server returned '{0}' with the status code '{1} ({2:d})'.",
-                            err.StatusDescription, err.StatusCode, err.StatusCode);
-                    }
-                }
-            }
-            finally
-            {
-                if (response != null) { response.Close(); }
-            }
-
+            ViewBag.OAuthResult = accessToken;
+            ViewBag.MeCoursesResult = PearsonApiUtilities.XAuthApiCall(accessToken, "http://m-api.ecollege.com/me/courses");
+            ViewBag.CourseDetails = PearsonApiUtilities.XAuthApiCall(accessToken, "https://m-api.ecollege.com/courses/3312999");
 
             return View();
-
         }
 
-        public string XAuthHeaderTest(string token)
-        {
-            // Build the X-Authorization request header
-            string xauth = String.Format("X-Authorization: Access_Token access_token={0}", token);
 
-            HttpWebResponse response = null;
 
-            try
-            {
-                // Setup the Request
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://m-api.ecollege.com/me/courses");
-                request.Method = "GET";
-                request.Headers.Add(xauth);
-
-                // Set the request body if making a POST or PUT request
-                //if (httpMethod == "POST" || httpMethod == "PUT")
-                //{
-                //    byte[] dataArray = Encoding.UTF8.GetBytes(body);
-                //    request.ContentLength = dataArray.Length;
-
-                //    Stream requestStream = request.GetRequestStream();
-                //    requestStream.Write(dataArray, 0, dataArray.Length);
-                //    requestStream.Close();
-                //}
-
-                // Send Request & Get Response
-                 response = (HttpWebResponse)request.GetResponse();
-
-                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-                {
-                    // Get the response stream and write to console
-                    string json = reader.ReadToEnd();
-                    return json;
-                }
-            }
-            catch (WebException e)
-            {
-                // This exception will be raised if the server didn't return 200 - OK
-                // Retrieve more information about the error
-                if (e.Response != null)
-                {
-                    using (HttpWebResponse err = (HttpWebResponse)e.Response)
-                    {
-                        return string.Format("The server returned '{0}' with the status code '{1} ({2:d})'.",
-                            err.StatusDescription, err.StatusCode, err.StatusCode);
-                    }
-                }
-                return e.Message;
-            }
-            finally
-            {
-                if (response != null) { response.Close(); }
-            }
- 
-        }
     }
 }
---
+++ Whisper.API/Whisper.API/Utilities/PearsonApiUtilities.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace Whisper.API.Utilities
+{
+    public static class PearsonApiUtilities
+    {
+        public static string GetOauthAccessToken(string username, string password)
+        {
+            const string grantType = "password";
+            const string clientID = "814c9ce7-664a-412f-a830-ff96269b0613";
+            const string clientString = "sullyu";
+            const string url = "https://m-api.ecollege.com/token";
+
+            // Create the request data
+            var data = new StringBuilder();
+            data.Append("grant_type=" + Uri.EscapeDataString(grantType));
+            data.Append("&client_id=" + Uri.EscapeDataString(clientID));
+            data.Append("&username=" + Uri.EscapeDataString(clientString + "\\" + username));
+            data.Append("&password=" + Uri.EscapeDataString(password));
+
+            // Create a byte array of the data to be sent
+            byte[] byteArray = Encoding.UTF8.GetBytes(data.ToString());
+
+            // Setup the Request
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+
+            // Write data
+            Stream postStream = request.GetRequestStream();
+            postStream.Write(byteArray, 0, byteArray.Length);
+            postStream.Close();
+
+            // Send Request & Get Response
+            var response = (HttpWebResponse)request.GetResponse();
+
+            string accessToken;
+
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                // Get the Response Stream
+                string json = reader.ReadLine();
+
+                // Retrieve and Return the Access Token
+                var ser = new JavaScriptSerializer();
+                var x = (Dictionary<string, object>)ser.DeserializeObject(json);
+                accessToken = x["access_token"].ToString();
+            }
+
+            return accessToken;
+        }
+
+
+        public static string XAuthApiCall(string token, string url)
+        {
+            // Build the X-Authorization request header
+            string xauth = String.Format("X-Authorization: Access_Token access_token={0}", token);
+
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.Headers.Add(xauth);
+
+                using (var response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string json = reader.ReadToEnd();
+                        return json;
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                // This exception will be raised if the server didn't return 200 - OK
+                // Retrieve more information about the error
+                if (e.Response != null)
+                {
+                    using (HttpWebResponse err = (HttpWebResponse)e.Response)
+                    {
+                        return string.Format("The server returned '{0}' with the status code '{1} ({2:d})'.",
+                            err.StatusDescription, err.StatusCode, err.StatusCode);
+                    }
+                }
+                return e.Message;
+            }
+
+
+        }
+    }
+}
