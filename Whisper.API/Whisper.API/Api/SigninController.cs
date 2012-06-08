c7077213c8e2c7a2a5893ab65385f5e0f0e8c8fb
Eric Bowen <github@scrappydog.com>
2012-06-07 21:32:01 -0600

    if at first you don't succeed...

--- Whisper.API/Whisper.API/Api/SigninController.cs
+++ Whisper.API/Whisper.API/Api/SigninController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using Whisper.API.Models;
@@ -27,6 +28,7 @@ namespace Whisper.API.Controllers
             //var result = StudentUtilities.GetStudentPoco("User0001");
 
             //result.UserName = username; //for testing purposes only...
+           HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
 
             return userJson;
         }
